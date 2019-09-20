4bf4d03b00a6a6ea3a05af05028eced75817d09e
Omulosi <mulongojohnpaul@gmail.com>
2019-09-20 11:47:49 +0300

    Add routes to character and home pages. Update navigation styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 
 import Header from "./components/Header.js";
 import SearchForm from './components/SearchForm';
+import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
 
 
 const fetchAPI = 'https://rick-api.herokuapp.com/api/';
@@ -19,11 +21,12 @@ export default function App() {
       <div className="navigation">
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/characters">Characters</Link></li>
+          <li><Link to="/characters">CharacterList</Link></li>
         </ul>
         <SearchForm />
-
       </div>
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </main>
   );
 }
